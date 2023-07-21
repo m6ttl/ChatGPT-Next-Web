3a93d9d1657d287638569cc45975c4a1553e75b6
m6ttl <41244955+m6ttl@users.noreply.github.com>
2023-07-21 18:39:12 +0800

    Update layout.tsx
    标题修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Ewide 人工智能",
+  description: "智慧水务行业专家.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Ewide 人工智能",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
